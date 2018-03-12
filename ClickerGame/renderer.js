7c24b86f9541f739ed7ea32a2472ad0f6d1f59bd
jibblol <jibblol@gmail.com>
2018-03-12 23:09:44 +0100

    Added clicker function and basic upgrade

--- ClickerGame/renderer.js
+++ ClickerGame/renderer.js
@@ -7,3 +7,13 @@ function incrementValue(){
 
     console.log('Called! :))))')
 }
+
+function buyUpgrade(){
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value>=5){
+        value-=5;
+        document.getElementById('number').value = value;
+    }
+    console.log("Bought Upgrade");
+}
