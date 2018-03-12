13a613c1d7eaa9724a51dfe7964c3fc8a96db6cf
jibblol <jibblol@gmail.com>
2018-03-12 23:50:05 +0100

    Added counter for number of upgrades bought

--- ClickerGame/renderer.js
+++ ClickerGame/renderer.js
@@ -11,9 +11,18 @@ function incrementValue(){
 function upgradeOne(){
     var value = parseInt(document.getElementById('number').value, 10);
     value = isNaN(value) ? 0 : value;
+    console.log("Bought Upgrade");
     if(value>=5){
         value-=5;
         document.getElementById('number').value = value;
+        upgradeCount();
     }
-    console.log("Bought Upgrade");
+}
+
+function upgradeCount(){
+    var value = parseInt(document.getElementById('sidenumber').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('sidenumber').value = value;
+    console.log("upgrade +1");
 }
